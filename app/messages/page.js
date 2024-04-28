434c0535b44cd2a5112be136003641c89ea75d11
Aleksandar <aleksandar@e-vlasotince.info>
2024-04-29 01:39:12 +0200

    using cache from react, unstabile_cache from next

--- app/messages/page.js
+++ app/messages/page.js
@@ -4,7 +4,7 @@ import { getMessages } from "@/lib/messages";
 //export const revalidate = 5;
 //export const dynamic = "force-dynamic"; // cache:"no-store"
 
-export default function MessagesPage() {
+export default async function MessagesPage() {
   // const response = await fetch("http://localhost:8080/messages", {
   //   next: {
   //     tags: ["msg"],
@@ -12,7 +12,7 @@ export default function MessagesPage() {
   // });
   // const messages = await response.json();
 
-  const messages = getMessages();
+  const messages = await getMessages();
 
   if (!messages || messages.length === 0) {
     return <p>No messages found</p>;
